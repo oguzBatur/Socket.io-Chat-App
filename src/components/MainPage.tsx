0a9ba108c6a9d587159fb43dafe1fe617cb41460
oguzBatur <sariozbatur@gmail.com>
2021-12-30 14:11:51 +0300

    Changed the name

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import {io} from 'socket.io-client';
 
-const socket = io('http://localhost:3004');
-socket.on('connect', () => {
-    socket.emit('custom-event');
-
-})
 export default function MainPage() {
     return (
         <div className='flex items-center  text-white justify-center h-5/6 flex-col gap-3'>
